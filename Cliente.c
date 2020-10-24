2791c6a43d86793e46e2637683a8322083938319
Jorge <jorgeedugutima15@gmail.com>
2020-10-24 08:57:21 -0600

    Cliente manual listo y job scheduler almost done

--- Cliente.c
+++ Cliente.c
@@ -4,53 +4,88 @@
 #include <string.h> 
 #include <sys/socket.h> 
 #include <pthread.h>
-#define MAX 4096
+#include <time.h> 
+#define MAX __SIZEOF_INT__*3
 #define PORT 8087 
 #define SA struct sockaddr 
+#define OK       0
+#define NO_INPUT 1
+#define TOO_LONG 2
+#define NTHREADS 64
 
-void readFile(int sockfd) 
-{ 
-	char buff[MAX]; 
-	int n; 
-	bzero(buff, sizeof(buff)); 
-	printf("Enter the path : "); 
-	n = 0; 
-	while ((buff[n++] = getchar()) != '\n') 
-		; 
+pthread_mutex_t lock= PTHREAD_MUTEX_INITIALIZER;
+struct socketInfo{ 
+    int sfd; 
+	char* b;
+};
+void* ThreadLogic(char* data );
 
-	fp = fopen(buff, "r");
-    if(fp == NULL){
-        printf("Error(open): %s\n", buffer);        
-        return;
+static int getLine (char *prmpt, char *buff, size_t sz) {
+    int ch, extra;
+
+    // Get line with buffer overrun protection.
+    if (prmpt != NULL) {
+        printf ("%s", prmpt);
+        fflush (stdout);
     }
-	
-    while (fgets(buff, MAX, fp) != NULL){
-		//aquí va el thread
-		pthread_t t;	
-		pthread_create(&t,NULL,ThreadLogic, buff);		
-	}
+    if (fgets (buff, sz, stdin) == NULL)
+        return NO_INPUT;
 
-    fclose(fp);
-    return;
-} 
-
-void* ThreadLogic(char* buffer ){
-	char buff= *buffer;
-	write(sockfd, buff, sizeof(buff)); 
-   		 if ((strncmp(buff, "exit", 4)) == 0) { 
-		printf("Client Exit...\n"); 
-		return;
-		}
-		bzero(buff, sizeof(buff)); 
-		read(sockfd, buff, sizeof(buff)); 
-		printf("From Server : %s", buff); 
+    // If it was too long, there'll be no newline. In that case, we flush
+    // to end of line so that excess doesn't affect the next call.
+    if (buff[strlen(buff)-1] != '\n') {
+        extra = 0;
+        while (((ch = getchar()) != '\n') && (ch != EOF))
+            extra = 1;
+        return (extra == 1) ? TOO_LONG : OK;
+    }
+    // Otherwise remove newline and give string back to caller.
+    buff[strlen(buff)-1] = '\0';
+    return OK;
 }
 
-int main() 
-{ 
+void ClienteManual(char* path) 
+{ 	
+	
+	FILE *fp;
+	fp = fopen(path, "r");
+    if(fp == NULL){
+        printf("Error(open): %s\n", path);        
+        return;
+    }			
+	char line[4];
+	pthread_t threads[NTHREADS];
+  	void * retvals[NTHREADS];	
+	char* lines[NTHREADS];
+	int pos =0;
+    while (fgets(line, MAX, fp) != NULL){
+		if (line[0] != 'b' && line[0] != 'B'){						
+			lines[pos] = (char *) malloc(4);
+			strncpy(lines[pos],line,4);
+			printf("He leido: %s\n",lines[pos]);			
+			pthread_create(&threads[pos],NULL,ThreadLogic,lines[pos]);				
+			pos++;
+		}		
+		time_t t;
+		srand((unsigned) time(&t));
+		int num = (rand() %(8 - 3 + 1)) + 3; 	
+		sleep(num); 
+	}
+	fclose(fp);
+
+	for (int i = 0; i < pos; i++){
+      if (pthread_join(threads[i], &retvals[i]) != 0)
+        {
+          fprintf(stderr, "error: Cannot join thread # %d\n", i);
+        }
+    }    
+    return NULL;
+} 
+
+void* ThreadLogic(char* data){	
 	int sockfd, connfd; 
 	struct sockaddr_in servaddr, cli; 
-
+	srand(time(0));
 	// socket create and varification 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
 	if (sockfd == -1) { 
@@ -67,16 +102,53 @@ int main()
 	servaddr.sin_port = htons(PORT); 
 
 	// connect the client socket to server socket 
-	if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) { 
+	if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) { // C
 		printf("connection with the server failed...\n"); 
 		exit(0); 
 	} 
 	else
 		printf("connected to the server..\n"); 
+	//--------------------
+	int rest =0;
+	printf("He leido del thread: %s\n",data);
+	sleep(2); //los 2 segundos de sleep antes de enviar los datos.
+	write(sockfd, data, sizeof(data)); 
+	//recibimiento
+	
+	read(sockfd, data, sizeof(data)); 
+	printf("From Server : %s\n", data);
+	close(sockfd);
 
-	// function for readFile 
-	readFile(sockfd); 
+}
 
+int main() 
+{ 
+	
+	
+	//Menú de selección de cliente
+	int rc;
+    char buff[2];
+
+    rc = getLine ("Enter 1 for manual client or 2 for automatic client: ", buff, sizeof(buff));
+    if (rc == NO_INPUT) {
+        // Extra NL since my system doesn't output that on EOF.
+        printf ("\nNo input, try again.\n");
+        
+    }
+
+    if (rc == TOO_LONG) {
+        printf ("Input too long, try again [%s]\n", buff);
+        
+    }
+    if (buff[0]== '1'){
+        printf ("You select #%s the manual client.\n", buff);    		
+    	char path[4096];
+		rc = getLine ("Enter the path of the process file: ", path, sizeof(path));
+		ClienteManual(path);
+    }
+    
 	// close the socket 
-	close(sockfd); 
+	
+	return 0;
 } 
+//ṕara ver threads ps -a -T 
--- Server.c
+++ Server.c
@@ -8,22 +8,29 @@
 #include <string.h> 
 #include <sys/socket.h> 
 #include <sys/types.h> 
-
+#include <pthread.h>
 #define PORT 8087
-#define MAX __SIZEOF_INT__*3
+#define MAX 4
 #define SOCKETEROR (-1)
 #define SERVER_BACKLOG 12 //Cantidad máxima de conexiones
+#define NTHREADS 64
 
 typedef struct sockaddr_in SA_IN;
 typedef struct sockaddr SA;
-
-struct process{
+int TiempoGlobal =0;
+int CPUOcioso =0;
+int cantProcesos=0;
+// queue Ready;
+// queue finished;
+struct PCB{
     int PID;
     int burst;
     int prioridad;
+    int tLlegada;
+    int tSalida;
 };
 
-void handle_connection(int client_Socket);
+void* job_scheduler(void* client_Socket);
 int check(int exp, const char* mjs);
 
 
@@ -33,79 +40,75 @@ int main()
 	int server_socket, client_socket, addr_size;
     SA_IN server_addr, client_addr;
     //Create socket verification
-    check((server_socket = socket(AF_INET, SOCK_STREAM, 0)), "failed to create socket") 	
+    check((server_socket = socket(AF_INET, SOCK_STREAM, 0)), "failed to create socket");
 	printf("Socket successfully created..\n"); 
-	//bzero(&servaddr, sizeof(servaddr)); // limpiamos la estructura del serverAdress, ojo que recibe la drección de memoria y la cantidad de lo que limpia
+	//bzero(&server_addr, sizeof(servaddr)); // limpiamos la estructura del serverAdress, ojo que recibe la drección de memoria y la cantidad de lo que limpia
 
 	// assign IP, PORT 
-	servaddr.sin_family = AF_INET; 
-	servaddr.sin_addr.s_addr = INADDR_ANY; 
-	servaddr.sin_port = htons(PORT); 
+	server_addr.sin_family = AF_INET; 
+	server_addr.sin_addr.s_addr = INADDR_ANY; 
+	server_addr.sin_port = htons(PORT); 
 
 	// Binding newly created socket to given IP and verification 
-	check((bind(server_socket, (SA*)&server_addr, sizeof(server_addr)), "bind feiled..")
+	check((bind(server_socket, (SA*)&server_addr, sizeof(server_addr))), "bind feiled..");
     printf("Socket successfully binded..\n"); 
 
 	// Now server is ready to listen and verification 
-    check((listen(server_socket, SERVER_BACKLOG)),"Listen failed")
-	printf("Server listening..\n"); 
-	
+    check((listen(server_socket, SERVER_BACKLOG)),"Listen failed");
+	printf("Server listening..\n"); 		
     while(true){
-        printf("waiting for connections");
+        printf("waiting for connections\n");
         //wait for, and eventually accept an incoming connection
         addr_size = sizeof(SA_IN);
-        check(client_socket = accept(server_socket, (SA*)&client_addr, (socklen_t*)&addr_size) ,"Accept failed");
-        printf("Connected");;
-        // hacer nuestra lógica con conexiones
-        handle_connection(client_socket);
+        check(client_socket = 
+                accept(server_socket, (SA*)&client_addr, (socklen_t*)&addr_size) ,
+                "Accept failed");
+        printf("Connected\n");    
+        // hacer nuestra lógica con conexiones        
+        pthread_t t;
+        int*pclient = malloc(sizeof(int));
+        *pclient = client_socket;        
+        pthread_create(&t,NULL,job_scheduler,pclient);
+        //Thread del CPU Scheduler
     }
     return 0;
 } 
-int check(int exp, cont char *mjs){
+int check(int exp, const char *mjs){
     if(exp == SOCKETEROR){
         perror(mjs);
         exit(1);
     }
     return exp;
 }
-void handle_connection(int client_Socket){
+void* job_scheduler (void* p_client_Socket){
+    
+    int client_Socket = *((int*)p_client_Socket);
+    free(p_client_Socket); //We don't need it anymore..
     char buffer[MAX];
     size_t bytes_read;
     int mjsSize= 0;
-    char actualpath[PATH_MAX+1]
+    //char actualpath[PATH_MAX+1];
     //leemos el mjs del cliente
-    while (bytes_read = read(client_Socket, buffer+mjsSize, sizeof(buffer)-mjsSize-1) >0 )
+
+    while (bytes_read = read(client_Socket, buffer, sizeof(buffer)) >0 )
     {
-        mjsSize += bytes_read;
-        if(mjsSize > BUFSIZE-1 || buffer[mjsSize-1]== "\n") break;        
+        check(bytes_read, "Recv error");
+        buffer[4] = '\0'; // terminate the mjs and remove the enter
+        printf ("\nREQUEST: %s\n",buffer);
+        fflush(stdout);
+        printf("Esto se envía al cliente: %s\n",buffer);
+        write(client_Socket,buffer,sizeof(buffer));    
     }
-    check(bytes_read, "Recv error");
-    buffer[mjsSize-1] = 0; // terminate the mjs and remove the enter
-    printf ("REQUEST: %s\n",buffer);
-    fflush(stdout);
-
-    if(realpath(buffer,actualpath) == NULL){
-        printf("Error(bad path): %s\n", buffer);
-        close(client_Socket);
-        return;
-    }
-
-    //lee el path del cliente
-    FILE *fp = fopen(actualpath, "r");
-    if(fp == NULL){
-        printf("Error(open): %s\n", buffer);
-        close(client_Socket);
-        return;
-    }
-
-    //leemos y se lo enviamos al cliente
-    while ( (bytes_read = fread(buffer,1,BUFSIZE,fp)) > 0){
-        printf("Sending %zu bytes\n", bytes_read);
-        write(client_Socket,buffer,bytes_read);
-    }
+    // read(client_Socket, buffer, sizeof(buffer));
+    // buffer[4] = '\0'; // terminate the mjs and remove the enter
+    // printf ("\nREQUEST: %s\n",buffer);
+    // fflush(stdout);
+    // printf("Esto se envía al cliente: %s\n",buffer);    
+    // write(client_Socket,buffer,sizeof(buffer));    
+    //Aquí viene el buffer con los datos brust prioridad, se le deba dar el pci al proceso...
+    
     close(client_Socket);
-    fclose(fp);
     printf("closing connection\n");
-
+    return NULL;
     
 }
